9eb08c5586ecc261ecee401188a6b4b1020d8967
Fabricio Santos <fbsantos0172020@gmail.com>
2024-01-06 13:11:19 -0300

    Desafio concluidogit status!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+// limpar console
+Console.Clear();
+
+Console.WriteLine("Smartphone nokia:");
+
+Smartphone nokia = new Nokia("00000-0000", "Nokia", "909090909090", 64);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+// Espacamento no console
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iphone:");
+
+Smartphone iphone = new Iphone("11111-1111", "Apple", "707070707070", 32);
+
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
